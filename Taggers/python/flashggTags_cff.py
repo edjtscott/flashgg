0a37a8e55e44fc9db048e13566872ed9d20c1ad3
Edward Scott <e.scott15@ic.ac.uk>
2021-01-20 11:32:12 +0100

    align labels between config and tagger

--- Taggers/python/flashggTags_cff.py
+++ Taggers/python/flashggTags_cff.py
@@ -318,8 +318,6 @@ flashggTHQLeptonicTag = cms.EDProducer("FlashggTHQLeptonicTagProducer",
 )
 
 flashggTHQHadronicTag = cms.EDProducer("FlashggTHQHadronicTagProducer",
-                                       DiPhotonName=cms.string('flashggPreselectedDiPhotons'),
-                                       DiPhotonSuffixes = cms.vstring(''), # nominal and systematic variations
                                        JetsName=cms.string('flashggUnpackedJets'),
                                        SystematicsJetsName=cms.string('flashggJetSystematics'),
                                        JetsCollSize = cms.uint32(maxJetCollections),
@@ -327,7 +325,6 @@ flashggTHQHadronicTag = cms.EDProducer("FlashggTHQHadronicTagProducer",
                                        MetName=cms.string('flashggMets'),
                                        SystematicsMetName=cms.string('flashggMetSystematics'),
                                        MetSuffixes = cms.vstring(''), # nominal and systematic variations
-                                       MVAResultName=cms.string('flashggDiPhotonMVA'),
                                        DiPhotonTag=cms.InputTag('flashggPreselectedDiPhotons'),
                                        SystLabel=cms.string(""),
                                        MVAResultTag=cms.InputTag('flashggDiPhotonMVA'),
@@ -374,6 +371,8 @@ flashggTHQHadronicTag = cms.EDProducer("FlashggTHQHadronicTagProducer",
                                        ElePhotonDrCut = cms.double(0.),
                                        ElePhotonZMassCut = cms.double(5),
                                        DeltaRTrkEle = cms.double(0.),
+                                       thqVsBkgMvaThreshold = cms.double(-1.),
+                                       thqVsTTHMvaThreshold = cms.double(-1.),
                                        debug = cms.bool(False)
                                        )
 
